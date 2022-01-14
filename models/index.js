85283558ade253e613a1d918456472a1023ae15e
Edison Simondet <edison.simondet@gmail.com>
2022-01-13 19:51:03 -0600

    created association methods for models

--- models/index.js
+++ models/index.js
@@ -6,11 +6,24 @@ const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
 
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+});
+
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'product_id',
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(ProductTag, {
+  foreignKey: 'tag_id',
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(ProductTag, {
+  foreignKey: 'tag_id',
+});
 
 module.exports = {
   Product,
