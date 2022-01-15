14f154121f71c6ddf5cc66aaa43e970e291e9fd5
Edison Simondet <edison.simondet@gmail.com>
2022-01-15 09:46:08 -0600

    updated rag-routes

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -5,24 +5,88 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 router.get('/', (req, res) => {
   // find all tags
-  // be sure to include its associated Product data
+  Tag.findAll({
+    include: [
+      {
+        model: Product,
+      },
+    ],
+  })
+    .then((dbProductData) => {
+      res.json({ data: dbProductData });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 router.get('/:id', (req, res) => {
   // find a single tag by its `id`
-  // be sure to include its associated Product data
+  Tag.findOne({
+    where: { id: req.params.id },
+    include: [
+      {
+        model: Product,
+      },
+    ],
+  })
+    .then((dbProductData) => {
+      res.json({ data: dbProductData });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 router.post('/', (req, res) => {
   // create a new tag
+  /* req.body expects:
+    {
+      "tag_name": "indie"
+    }
+  */
+  Tag.create(req.body)
+    .then((tag) => {
+      res.status(200).json(tag);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(400).json(err);
+    });
 });
 
 router.put('/:id', (req, res) => {
   // update a tag's name by its `id` value
+  // update tag data
+  Tag.update(req.body, {
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((updatedTag) => res.json(updatedTag))
+    .catch((err) => {
+      // console.log(err);
+      res.status(400).json(err);
+    });
 });
 
 router.delete('/:id', (req, res) => {
   // delete on tag by its `id` value
+  Tag.destroy({
+    where: { id: req.params.id },
+  })
+    .then(() => {
+      console.log(
+        `Product with ID: ${req.params.id} was successfully deleted!`
+      );
+      res.json(`Product with ID: ${req.params.id} was successfully deleted!`);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 module.exports = router;
